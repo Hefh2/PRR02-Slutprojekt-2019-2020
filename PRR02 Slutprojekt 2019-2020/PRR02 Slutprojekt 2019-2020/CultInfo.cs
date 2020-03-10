a60fb1c50bef2b64ae2badfb4abd7c138d84f855
Hefh2 <henning.joelsson@elev.ga.ntig.se>
2020-03-10 13:31:20 +0100

    Bro

---
+++ PRR02 Slutprojekt 2019-2020/PRR02 Slutprojekt 2019-2020/CultInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRR02_Slutprojekt_2019_2020
+{
+    class CultInfo
+    {
+        public string cultName;
+        public int treasure;
+        public int supplies;
+        public int happiness;
+        public int population;
+        public int offerings;
+        public static Random generator = new Random();
+
+        public CultInfo()
+        {
+            treasure = generator.Next(200, 601);
+            supplies = generator.Next(200, 601);
+            happiness = generator.Next(200, 601);
+            population = generator.Next(20, 61);
+            offerings = generator.Next();
+
+        }
+
+
+    }
+}
---
+++ PRR02 Slutprojekt 2019-2020/PRR02 Slutprojekt 2019-2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRR02_Slutprojekt_2019_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
